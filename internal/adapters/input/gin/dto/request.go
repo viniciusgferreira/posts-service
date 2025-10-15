c09075f2faf688ef396c0e607731f17025c4e9c9
Vinicius Ferreira <vinicius.gomesferreira@wexinc.com>
2025-10-15 06:51:22 -0300

    add PUT to update post

--- internal/adapters/input/gin/dto/request.go
+++ internal/adapters/input/gin/dto/request.go
@@ -12,3 +12,11 @@ type CreatePostRequest struct {
 type GetPostRequest struct {
 	ID string `uri:"id" binding:"required,min=1" example:"123456789"`
 }
+
+// UpdatePostRequest represents the request payload for updating an existing post
+type UpdatePostRequest struct {
+	Title           string `json:"title" binding:"required,min=1,max=200" example:"O Guia Completo para Arquitetura Hexagonal"`
+	AuthorID        string `json:"author_id" binding:"required,min=1" example:"987654321"`
+	CoverImageURL   string `json:"cover_image_url" binding:"omitempty,url" example:"https://cdn.seu-blog.com/imagens/post-arquitetura-hexagonal-capa.png"`
+	MarkdownContent string `json:"markdown_content" binding:"required,min=1" example:"# Guia para Arquitetura Hexagonal..."`
+}
