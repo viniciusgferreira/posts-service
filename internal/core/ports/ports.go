c4e2b5f5d9c504fcb1c232b4a62e69766473aa79
Vinicius Ferreira <vinicius.gomesferreira@wexinc.com>
2025-10-16 12:05:21 -0300

    remove use cases interface

--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -17,12 +17,3 @@ type AuthorRepository interface {
 	Update(author *domain.Author) error
 	Delete(id string) error
 }
-
-// TODO: This will be implemented in the use cases layer
-type PostUseCases interface {
-	CreatePost(title, content, authorID, coverImageURL string) (*domain.Post, error)
-	GetPost(id string) (*domain.Post, error)
-	GetAllPosts() ([]*domain.Post, error)
-	UpdatePost(id, title, content, coverImageURL string) (*domain.Post, error)
-	DeletePost(id string) error
-}
