b9a52b9d6eb1a6bc1ba3b649bec64ed506c69eb4
Vinicius Ferreira <vinicius.gomesferreira@wexinc.com>
2025-10-16 10:19:09 -0300

    remove unused fields from author

--- internal/core/domain/author.go
+++ internal/core/domain/author.go
@@ -3,15 +3,12 @@ package domain
 import (
 	"errors"
 	"strings"
-	"time"
 )
 
 type Author struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name"`
-	Email     *Email    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email *Email `json:"email"`
 }
 
 func NewAuthor(id, name, email string) (*Author, error) {
@@ -24,12 +21,9 @@ func NewAuthor(id, name, email string) (*Author, error) {
 		return nil, err
 	}
 
-	now := time.Now()
 	return &Author{
-		ID:        id,
-		Name:      strings.TrimSpace(name),
-		Email:     validatedEmail,
-		CreatedAt: now,
-		UpdatedAt: now,
+		ID:    id,
+		Name:  strings.TrimSpace(name),
+		Email: validatedEmail,
 	}, nil
 }
