bab20376d627f003e872afc674792397531e7776
Vinicius Ferreira <vinicius.gomesferreira@wexinc.com>
2025-10-17 17:33:06 -0300

    rename vo post

--- internal/core/domain/post.go
+++ internal/core/domain/post.go
@@ -8,14 +8,14 @@ import (
 )
 
 type Post struct {
-	ID              string                        `json:"id"`
+	ID              string              `json:"id"`
 	Title           *vo.Title           `json:"title"`
 	Slug            *vo.Slug            `json:"slug"`
-	Author          *Author                       `json:"author"`
+	Author          *Author             `json:"author"`
 	CoverImageURL   *vo.CoverImageURL   `json:"cover_image_url"`
 	MarkdownContent *vo.MarkdownContent `json:"markdown_content"`
-	CreatedAt       time.Time                     `json:"created_at"`
-	UpdatedAt       time.Time                     `json:"updated_at"`
+	CreatedAt       time.Time           `json:"created_at"`
+	UpdatedAt       time.Time           `json:"updated_at"`
 }
 
 func NewPost(id, title, markdownContent string, author *Author, coverImageURL string) (*Post, error) {
